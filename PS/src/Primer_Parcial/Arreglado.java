23105147b740bc1303a6f16c8a08939daa9205dc
Israelote123 <joseprinsisrael@gmail.com>
2022-02-17 22:48:46 -0600

    Agrego impresion de tabla

--- PS/src/Primer_Parcial/Arreglado.java
+++ PS/src/Primer_Parcial/Arreglado.java
@@ -7,7 +7,7 @@ public class Arreglado {
     }
 
     private static void ciclo() {
-        final int numeros = 3000;
+        final int numeros = 100;
         int [] numeroObtenido = new int [numeros];
         int[][] arreglo = new int [20][2];
         int n = 0;
@@ -19,14 +19,29 @@ public class Arreglado {
         }
 
         for(int i = -10; i < 11; i++){
-            lugar++;
             for (int x = 0; x < numeros; x++) {
                 if(i == numeroObtenido[x]){
                     contador++;
                 }
             }
-            arreglo[lugar][1] = i;
-            arreglo[lugar][2] = contador;
+            arreglo[lugar][0] = i;
+            arreglo[lugar][1] = contador;
+            if(lugar < 19){
+                lugar++;
+            }
         }
+
+        for(int i = 0; i < numeros; i++){
+            System.out.print(i + "  \t");
+        }
+            System.out.print("\n");
+        for(int i = 0; i < 20; i++){
+            System.out.print(arreglo[i][0] + "  \t");
+            for(int x = 0; x < arreglo[i][1] - 1; x++){
+                System.out.print("  \t");
+            }
+            System.out.print("X\n");
+        }
+
     }
 }
